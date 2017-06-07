81f248aa107907622d6bf70ab274a068d2c92cff
Luke Wilimitis <wilimitis9@gmail.com>
2017-06-07 00:17:18 -0500

    Serve index bundle from homepage

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+router.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
 module.exports = router;
